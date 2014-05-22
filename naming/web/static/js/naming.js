ae89b92ee5ae06dbbcac5ff3441c39597ed524b4
Kang HyoJun <admire9@gmail.com>
2014-05-22 22:31:04 +0900

    완전 js버전

--- naming/web/static/js/naming.js
+++ naming/web/static/js/naming.js
@@ -1,23 +1,46 @@
-String.prototype.namingify = function() {
+var namingify = function(name) {
+  var now = new Date();
+
+  name += now.toGMTString();
 
   var ENV = ['용기', '교만', '시기', '분노', '나태', '탐욕', '식탐', '색욕',
              '사랑', '증오', '졸림', '기쁨', '환호', '우울', '빡침' ],
-      name = this,
       hexed = name.toHex().split(''),
-      n = [];
+      n = [],
+      my_env = [];
   for(var i=0; i < hexed.length; i++) {
     n.push(hexed[i]);
     n.push(hexed[(hexed.length - 1) - i]);
   }
 
-  var qu = Math.floor(n.length / ENV.length),
+  var nl = 5 - h2d(n[0]) % 3,
+      l = Math.floor(n.length / nl),
       prob = [];
 
-  for(var i=0; i < n.length; i += qu) {
-    prob.push(n.slice(i, i + qu));
+  for(var i=0; i < n.length; i += l) {
+    prob.push(h2d(n.slice(i, i + l).join('')));
   }
 
-  console.log(prob);
+  sum = 0;
+  prob.map(function(item, i) {
+    sum += item;
+  });
 
-  return n;
+  prob.map(function(item, i) {
+    prob[i] = prob[i] / sum;
+  });
+
+  for(var i=0; i < nl; i++) {
+    my_env.push(ENV[h2d(n[i])]);
+  }
+
+  var result = [];
+  for(var i=0; i < nl; i++) {
+    result.push({
+      'feel': my_env[i],
+      'prob': prob[i]
+    });
+  }
+
+  return result;
 }
